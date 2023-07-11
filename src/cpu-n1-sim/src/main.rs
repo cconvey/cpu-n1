e73fc2817df431a62ff218a87a45c160ad23b776
Christian Convey <christian.convey@gmail.com>
2023-07-11 18:57:10 -0400

    progress

--- src/cpu-n1-sim/src/main.rs
+++ src/cpu-n1-sim/src/main.rs
@@ -1,3 +1,11 @@
+use std::io;
+
 fn main() {
-    println!("Hello, world!");
+    println!("Greetings, program.");
+
+    let mut cmd_string = String::new();
+
+    io::stdin().read_line(&mut cmd_string).expect("Failed to read line.");
+
+    println!("Command entered: {cmd_string} {:#p}", &cmd_string);
 }
